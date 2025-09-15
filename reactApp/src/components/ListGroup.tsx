82645f91fce701de4fde118e3683fee18cbf3409
agentP <learnhacksandtricks@gmail.com>
2025-09-14 22:17:43 -0700

    react

---
+++ reactApp/src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+function ListGroup() {
+  let items = ["New York", "California", "Tokyo", "Vancouver", "London"];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
