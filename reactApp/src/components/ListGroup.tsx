3888e25771ae687803b433f093979a567540f75d
agentP <learnhacksandtricks@gmail.com>
2025-09-14 22:20:22 -0700

    16 sept

--- reactApp/src/components/ListGroup.tsx
+++ reactApp/src/components/ListGroup.tsx
@@ -5,6 +5,7 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
+      {items.length == 0 ? <p>No items found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
