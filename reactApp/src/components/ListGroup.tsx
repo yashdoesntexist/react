17ee684f53183d25367acb75f25b21a33ca23cf0
agentP <learnhacksandtricks@gmail.com>
2025-09-19 21:41:38 -0700

    more react stuff

--- reactApp/src/components/ListGroup.tsx
+++ reactApp/src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
   const items = ["New York", "California", "Tokyo", "Vancouver", "London"];
+  // const message = items.length == 0 ? <p>No items found</p> : null;
+
+  const getMessage = () => {
+    return items.length == 0 && <p>No items found</p>;
+  };
   return (
     <>
       <h1>List</h1>
-      {items.length == 0 ? <p>No items found</p> : null}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
